aaf22fabe7d994ce0c6d1a42980c671fc5a42c96
Mehmet CETIN <mehmetcetins@hotmail.com>
2017-08-17 00:35:40 +0300

    quick-sort done

--- exampleClass.py
+++ exampleClass.py
@@ -16,3 +16,22 @@ class example:
         else : 
             m = int(((len(array) -1 ) / 2) + 1)
         return m
+
+    def quicksort(A,p,r):
+        if(p<r):
+            q=example.partition(A,p, r)
+            example.quicksort(A,p, q-1)
+            example.quicksort(A,q+1, r)
+        return A
+    def partition(A,p,r):
+        x = A[r]
+        i = p-1
+
+        for j in range(p,r):
+        
+            if(A[j]<=x):
+                i+=1
+                example.swit(i,j,A)
+            
+        example.swit(i+1,r,A)
+        return i+1
--- quickSort.py
+++ quickSort.py
@@ -1,26 +1,8 @@
 from exampleClass import example
 
-a = [26,33,35,29,19,12,22]
-
-p = a[0]
-a = example.swit(0,example.middle(a),a)
+a = [5,7,2,9,6,1,4,7]
 
 
-for i in range(0,len(a)):
-    p = a[0]
-    a = example.swit(0,example.middle(a),a)
-    if a[i] != p:
 
-        if a[i] < p:
-            a = example.swit(len(a) - i - 1,  i,a)
-        if a[-( i + 1 ) ] < p:
-            a = example.swit(len(a) - i - 1,  i,a)
-        if a[i] > p:
-            a = example.swit(len(a) - i - 1,  i,a)
-        if a[-( i + 1 )] > p:
-            a = example.swit(len(a) - i - 1,  i,a)
-p = a[0]
-a = example.swit(0,example.middle(a[0:example.middle(a)]),a)
-
-
-print (a)
+a = example.quicksort(a,0,len(a) - 1)
+print(a)
